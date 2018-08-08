a232d2df98cb424539391f105a71beba1d21366e
Halo Guan <doresimon@163.com>
2018-08-08 18:05:38 +0800

    add getParam(), getCookie()

--- public/html.js/func.js
+++ public/html.js/func.js
@@ -1,5 +1,24 @@
 const func = {
     jump: function(target){
         location.href = target
+    },
+    getParam: function(name) {
+        let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        let r = location.search.substr(1).match(reg);
+        let context = "";
+        if (r != null)
+            context = r[2];
+        reg = null;
+        r = null;
+        return context == null || context == "" || context == "undefined" ? "" : context;
+    },
+    getCookie:function(cname){
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for(let i=0; i<ca.length; i++) {
+            let c = ca[i].trim();
+            if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+        }
+        return null;
     }
 }
